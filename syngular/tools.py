1defce3ca8ad816e6c7c6768a3560f2c4c6737c3
Giuseppe De Laurentis <g.dl@hotmail.it>
2021-09-07 11:22:41 +0200

    Fix to communication when spawned as subprocess from terminal.

--- syngular/tools.py
+++ syngular/tools.py
@@ -10,9 +10,9 @@ def execute_singular_command(singular_command, timeout=syngular.TIMEOUT, verbose
     if len(singular_command) >= 131072:  # 2 ** 17
         with open("/tmp/.singular_command", "w") as file:
             file.write(singular_command)
-        test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", "/tmp/.singular_command"], stdout=subprocess.PIPE)
+        test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", "/tmp/.singular_command"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     else:
-        test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", "--execute", singular_command], stdout=subprocess.PIPE)
+        test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", "--execute", singular_command], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     output = test.communicate()[0]
     output = output.decode("utf-8")
     if output[-1] == "\n":
