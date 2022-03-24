03bf3885c0acdc4fe36e3897b67f3ebe78e00039
Giuseppe De Laurentis <g.dl@hotmail.it>
2022-03-24 20:25:30 +0100

    v0.1.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='syngular',
-    version='v0.1.1',
+    version='v0.1.2',
     license='GNU General Public License v3.0',
     description='An Object-Oriented Python Interface to Singular',
     long_description="""Documentation not yet available.
@@ -12,7 +12,7 @@ For now please refer to the examples at https://github.com/GDeLaurentis/syngular
     author='Giuseppe De Laurentis',
     author_email='g.dl@hotmail.it',
     url='https://github.com/GDeLaurentis/syngular',
-    download_url='https://github.com/GDeLaurentis/syngular/archive/v0.1.1.tar.gz',
+    download_url='https://github.com/GDeLaurentis/syngular/archive/v0.1.2.tar.gz',
     keywords=['syngular', 'singular', 'algebraic geometry'],
     packages=find_packages(),
     install_requires=['numpy',
