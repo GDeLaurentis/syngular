112b723a8319c2bf173e8d5c105a3b39171887e4
GDeLaurentis <g.dl@hotmail.it>
2024-05-01 19:41:32 +0100

    Significantly improved stability of parrallelised execution of long commands from Singular by not overwriting the same tmp file, and instead having random names. Cleanup happens at run time if no exception is thrown - this should allow to inspect buggy run files at /tmp/.singular_commands/ since these are the only one not cleaned up at run time(clean up of /tmp/ directory appears to be distro dependent).

--- syngular/tools.py
+++ syngular/tools.py
@@ -1,6 +1,9 @@
 import re
 import subprocess
 import syngular
+import random
+
+from pathlib import Path
 
 
 def execute_singular_command(singular_command, timeout=syngular.TIMEOUT, verbose=False):
@@ -8,9 +11,12 @@ def execute_singular_command(singular_command, timeout=syngular.TIMEOUT, verbose
         singular_command = "\n".join(singular_command)
     # print(singular_command)
     if len(singular_command) >= 131072:  # 2 ** 17
-        with open("/tmp/.singular_command", "w") as file:
+        Path("/tmp/.singular_commands/").mkdir(parents=True, exist_ok=True)
+        random_integer = random.randint(0, 2**64 - 1)
+        file_path = f"/tmp/.singular_commands/singular_command_{random_integer}"
+        with open(file_path, "w+") as file:
             file.write(singular_command)
-        test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", "/tmp/.singular_command"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", file_path], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     else:
         test = subprocess.Popen(["timeout", str(timeout), "Singular", "--quiet", "--execute", singular_command], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     output = test.communicate()[0]
@@ -35,6 +41,8 @@ def execute_singular_command(singular_command, timeout=syngular.TIMEOUT, verbose
         if verbose:
             print('Singular Warning: groebner base computations with inexact coefficients can not be trusted due to rounding errors')
         output = output.replace('// ** groebner base computations with inexact coefficients can not be trusted due to rounding errors\n', '')
+    if len(singular_command) >= 131072:
+        Path(file_path).unlink()
     return output
 
 
