7249ec421515a3e347f679748fefec3a26e094cd
GDeLaurentis <g.dl@hotmail.it>
2024-06-05 23:59:25 +0100

    Fixing setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-from .version import __version__ as version
+from version import __version__ as version
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
