e0a5794c41665273f37f6f8b7cba146228087bac
GDeLaurentis <g.dl@hotmail.it>
2025-04-02 10:14:26 +0100

    minor improvements to Point object, perhaps needs to be renamed to variety.

--- syngular/point.py
+++ syngular/point.py
@@ -2,9 +2,11 @@ import sympy
 
 from copy import deepcopy
 
+from .ideal import Ideal
+
 
 class RingPoint(dict):
-    """Represents a numerical or semi-numericla point in the variety associated to the ring.
+    """Represents a numerical or semi-numericla point on a variety within the space defiend by the ring.
     Generalizes the idea of a phase space point from particle physics.
     """
 
@@ -15,9 +17,9 @@ class RingPoint(dict):
     def __call__(self, string):
         return eval(string, {}, self)
 
-    def univariate_slice(self, seed=None):
+    def univariate_slice(self, indepSet=None, seed=None, verbose=False):
         t = sympy.symbols('t')
-        self.update(self.ring.univariate_slice(self.field, seed=seed)(t))
+        self.update(self.ring.univariate_slice(self.field, indepSet=indepSet, seed=seed, verbose=verbose)(t))
 
     def subs(self, myDict):
         for key, val in self.items():
@@ -28,3 +30,7 @@ class RingPoint(dict):
 
     def __hash__(self):
         return hash(frozenset(super().items()))
+
+    def singular_variety(self, directions=None, valuations=tuple()):  # to be improved
+        I = Ideal(self.ring, directions)
+        self.update(I.point_on_variety(field=self.field, directions=directions, valuations=valuations))
