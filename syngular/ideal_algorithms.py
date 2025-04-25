2b840721680addd1e7cba6441d81d762aab9990e
GDeLaurentis <g.dl@hotmail.it>
2025-04-25 14:17:34 +0100

    Cleaned sphinx conf.py, fixed missing if verbose around print statement in primdec test

--- syngular/ideal_algorithms.py
+++ syngular/ideal_algorithms.py
@@ -262,7 +262,8 @@ class Ideal_Algorithms:
                 if seminumerical_dim_computation:
                     valuations = [Polynomial(factor, field).subs(point) for point in points]
                     zero_count = sum(1 for valuation in valuations if valuation == 0)
-                    print(f"{zero_count} zeros out of {len(valuations)} points.")
+                    if verbose:
+                        print(f"{zero_count} zeros out of {len(valuations)} points.")
                     if any([valuation == 0 for valuation in valuations]):
                         return False if not astuple else (False, False)
                     else:
